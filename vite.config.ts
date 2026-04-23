8ec536e7c8e5120d0adb75b9cdab57e4a5a86bba
sammwy <sammwy.dev@gmail.com>
2026-04-23 11:13:20 -0300

    chore: remove local import of sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,9 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
-function getSDKDir() {
-    const path = require("path");
-    const fs = require("fs");
-    const maxDepth = 5;
-
-    for (let i = 0; i < maxDepth; i++) {
-        const sdkPath = path.join(__dirname, "../".repeat(i) + "sdk");
-        if (fs.existsSync(sdkPath) && fs.existsSync(path.join(sdkPath, "package.json"))) {
-            return sdkPath;
-        }
-    }
-
-    return null;
-}
-
-function resolveSDK(path: string) {
-    const sdkDir = getSDKDir();
-    if (!sdkDir) {
-        throw new Error("SDK not found");
-    }
-    return resolve(sdkDir, path);
-}
-
-
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
     base: "/widget/show-emote/",
     build: {
         outDir: "dist",
         emptyOutDir: true,
-    },
-    resolve: mode === "development" ? {
-        alias: {
-            "@staroverlay/sdk/tmi": resolveSDK("dist/tmi.mjs"),
-            "@staroverlay/sdk": resolveSDK("dist/staroverlay.mjs")
-        }
-    } : undefined
-}));
+    }
+});
